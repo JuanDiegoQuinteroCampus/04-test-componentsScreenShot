03a92a4e7913703cf15a2bc23076a14fa8fd29c4
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-20 06:17:36 -0500

    feat: :tada: 04-test-components
    Se inicio la configuracipón que tiene como prioridad tomar los screenshot con los testing

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Table from './components/Table'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Table />
+  </React.StrictMode>,
+)
